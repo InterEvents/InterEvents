776e44cd61aa0b9f386c9f7436d98d5c7b55e6ca
Afonso <afonso.martinho@hotmail.com>
2018-03-07 17:10:45 +0000

    Delete Register Button

--- app/src/main/java/com/example/ruigomes/interevents/Authentication.java
+++ app/src/main/java/com/example/ruigomes/interevents/Authentication.java
@@ -11,8 +11,7 @@ public class Authentication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
-
-//Comentario
+        //Comentario
 
 
     }
